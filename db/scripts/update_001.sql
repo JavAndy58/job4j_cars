07d833f059336d5ef670a624fd4d0dfc2272981f
JavAndy58 <digital.designs.russia@gmail.com>
2022-07-13 15:30:03 +0300

    Реализовать площадку продаж машин. [#4747]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,23 +1,38 @@
 CREATE TABLE engines(
     id SERIAL PRIMARY KEY,
-    name TEXT
+    name VARCHAR
 );
 
 CREATE TABLE cars(
     id SERIAL PRIMARY KEY,
-    name TEXT,
+    name VARCHAR ,
     engine_id INT NOT NULL UNIQUE REFERENCES engines(id)
 );
 
 CREATE TABLE drivers(
     id SERIAL PRIMARY KEY,
-    name TEXT
+    name VARCHAR
 );
 
 CREATE TABLE history_owner(
     id SERIAL PRIMARY KEY,
     driver_id INT NOT NULL REFERENCES drivers(id),
     car_id INT NOT NULL REFERENCES cars(id)
+);
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR
+);
+
+CREATE TABLE promos(
+    id SERIAL PRIMARY KEY,
+    description TEXT,
+    carBrand VARCHAR,
+    carBody VARCHAR,
+    photo BYTEA,
+    status BOOLEAN,
+    user_id INT NOT NULL REFERENCES users(id)
 );
 
 
--- src/main/java/ru/gromov/andrey/model/Promo.java
+++ src/main/java/ru/gromov/andrey/model/Promo.java
@@ -18,4 +18,63 @@ public class Promo {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    public Promo() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public String getCarBody() {
+        return carBody;
+    }
+
+    public void setCarBody(String carBody) {
+        this.carBody = carBody;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
